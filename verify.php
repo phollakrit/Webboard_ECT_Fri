0df0d8be6497e268b0d3a8a6fa454e249a7123e3
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-01-12 11:18:04 +0700

    work5 (php2)
    work5 (php2)

--- verify.php
+++ verify.php
@@ -9,14 +9,21 @@
     <h1 style="text-align: center;">Webboard KakKak</h1>
     <hr>
     <div style="text-align: center;">
-        เข้าสู่ระบบด้วย<br>
-        Login = <?php echo $_POST['login']; ?><br>
-        Password = <?php echo $_POST['pwd']; ?><br><br>
-
         <?php
-        echo "เข้าสู่ระบบด้วย<br>";
-        echo "Login = $_POST[login]<br>";
-        echo "Password = $_POST[pwd]";
+        $login=$_POST["login"];
+        $pwd=$_POST['pwd'];
+        if($login=="admin" && $pwd=="ad1234"){
+            echo "ยินดีต้อนรับคุณ ADMIN<br>";
+            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+        }
+        elseif($login=="member" && $pwd=="mem1234"){
+            echo "ยินดีต้อนรับคุณ MEMBER<br>";
+            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+        }else{
+            echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง<br>";
+            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+        }
+        
         ?>
     </div>
 </body>
