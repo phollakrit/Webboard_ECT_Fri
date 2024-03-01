f4a2dfcc4713542457750fbec817725b51473cf8
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-03-01 12:13:26 +0700

    Work9 Finished
    Work9 Finished

--- register_save.php
+++ register_save.php
@@ -1,23 +1,26 @@
 <?php
-if(isset($_POST['login'])){
-    $login=$_POST['login'];
-    $passwd=$_POST['pwd'];
-    $name=$_POST['name'];
-    $gender=$_POST['gender'];
-    $email=$_POST['email'];
+session_start();
+$login=$_POST['login'];
+$passwd=sha1($_POST['pwd']);
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
 
-    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
 
-    $sql="INSERT INTO user (login, password, name, gender, email, role)
-        VALUES ('$login','$passwd','$name','$gender','$email','m')";
-    $conn->exec($sql);
-    $conn=null;
-    header("location:login.php");
-    die();
+$sql="SELECT * FROM user where login='$login'";
+$result=$conn->query($sql);
+if ($result->rowCount()==1){
+    $_SESSION['add_login']="error";
 }else{
-    header("location:index.php");
-    die();
+    $sql="INSERT INTO user (login, password, name, gender, email, role)
+ VALUES ('$login','$passwd','$name','$gender','$email','m')";
+    $conn->exec($sql);
+    $_SESSION['add_login']="success";
 }
+$conn=null;
+header("location:register.php");
+die();
 ?>
 
 
