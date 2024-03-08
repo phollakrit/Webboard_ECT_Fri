0fdee15e67037687ba542199a855637a191e573f
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-03-08 12:23:01 +0700

    Work10 post.php and post_save.php
    Work10 post.php and post_save.php

--- post.php
+++ post.php
@@ -1,35 +1,70 @@
+<?php
+    session_start();    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>Post</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard KakKak</h1>
-    <hr> 
-    <div style="text-align: center;">
-        <?php
-        $id=$_GET['id'];
-        echo "ต้องการดูกระทู้หมายเลข $id<br>";
-        if ($id%2==0){
-            echo "เป็นกระทู้หมายเลขคู่";
-        }else{
-            echo "เป็นกระทู้หมายเลขคี่";
-        }
-        ?>        
-    </div>       
-    <br>
-    <table style="border: 2px solid black; width: 40%" align="center">
-        <tr style="background-color: #6cd2fe;">
-            <td>แสดงความคิดเห็น</td>
-        </tr>
-        <tr><td align="center">
-            <textarea cols="50" rows="10"></textarea><br>
-            <input type="submit" value="ส่งข้อความ">            
-        </td></tr>
-    </table>
-    <br>
-    <div style="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></div>
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
+        <?php include "nav.php" ?>
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+
+            <?php
+            $conn=new PDO("mysql:host=localhost;dbname=webboard;
+            charset=utf8","root","");
+            $sql="SELECT post.title,post.content,post.post_date,user.login
+             FROM post INNER JOIN user ON 
+            (post.user_id=user.id) WHERE post.id=$_GET[id]";
+            $result=$conn->query($sql);
+            while($row=$result->fetch()){
+            echo "<div class='card border-primary'>";
+            echo "<div class='card-header bg-primary text-white'>$row[title]</div>";
+            echo "<div class='card-body'>$row[content]<br><br>$row[3] - $row[2]</div>";
+            echo "</div>";
+            }
+            ?>
+
+            <div class="card border-success mt-3">
+                <label for="comment" class="card-header bg-success text-white">
+                    แสดงความคิดเห็น</label>
+                <div class="card-body">
+                    <form action="post_save.php" method="post">
+                        <input type="hidden" name="post_id" 
+                        value="<?= $_GET['id'];?>">
+                        <div class="row mb-3 justify-content-center">
+                            <div class="col-lg-10">
+                                <textarea id="comment" name="comment" rows="8" 
+                                class="form-control" required></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 d-flex justify-content-center">
+    <button type="submit" class="btn btn-success btn-sm text-white">
+                                        <i class="bi bi-box-arrow-up-right"></i> ส่งข้อความ
+                                    </button>
+                                    <button type="reset" class="btn btn-danger btn-sm ms-2">
+                                        <i class="bi bi-x-square"></i> ยกเลิก</button>                                
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+            <br>
+            </div>            
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+        </div>
+    </div>
 </body>
 </html>
+
